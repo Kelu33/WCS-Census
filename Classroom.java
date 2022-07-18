fad71d584e8bddfddd30046a864cf504326507c4
unknown <kelu33@gmail.com>
2022-07-18 11:44:26 +0200

    initial commit

---
+++ Classroom.java
@@ -0,0 +1,32 @@
+public class Classroom {
+
+    public static void main(String[] args) {
+        Wilder[] wilders = {
+                new Wilder("Adèle", false),
+                new Wilder("Anna", false),
+                new Wilder("Chloé", false),
+                new Wilder("Clément", false),
+                new Wilder("Iliess", false),
+                new Wilder("Jennifer", false),
+                new Wilder("Julien", false),
+                new Wilder("Lucas", false),
+                new Wilder("Numa", false),
+                new Wilder("Samuel", false),
+                new Wilder("Tatiana", false),
+                new Wilder("Thibault", false)
+        };
+
+        for (Wilder wilder : wilders) {
+            System.out.println(wilder.whoAmI());
+            wilder.setAware(true);
+        }
+
+        System.out.println("\n 6 months later :\n");
+
+        for (Wilder wilder : wilders) {
+            System.out.println(wilder.whoAmI());
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# WCS-CENSUS
+
+To compile and run please run in terminal :
+
+```
+javac Classroom.java
+java Classroom
+```
